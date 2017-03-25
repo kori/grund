95e9f46a2ffb44c0be56111283bc65c7e07003eb
Luiz de Milon <luiz.de.milon@gmail.com>
2017-03-25 02:27:15 -0300

    add racket version (lol)

---
+++ up.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+; knuth's up arrow notation
+(provide repeat
+         up)
+
+; n = number
+; c = counter
+(define (repeat n c)
+  (let aux ((in c) (out '()))
+    (if (zero? in)
+      out
+      (aux (- in 1) (cons n out)))))
+
+; d = depth
+; a^b = (up 1 a b)
+; a^^b = (up 2 a b) = (up (up 1 a b) a b)
+; a^^^b = (up 3 a b) = (up (up (up 1 a b) a b) a b)
+; and so on...
+(define (up d a b)
+  (fold-right
+    * 1 (repeat a (if (equal? 1 d)
+                    b
+                    (up (- d 1) a b)))))
